5ca799f63b039cf55677b9592713d3b41e2da083
ricardojvianna <comunicacao.vianna@gmail.com>
2025-01-21 02:21:40 -0300

    Colocando Som no icone Carro Senna

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 const cardFrame = document.querySelector("iframe")
 const cardMask = document.querySelector(".card-mask")
 
-function visibleFrame () {
+function visibleFrame() {
      cardFrame.style.left = "480px"
      cardMask.style.visibility = "visible"
 }
 
-cardMask.addEventListener("click", function() {
+cardMask.addEventListener("click", function () {
      cardFrame.style.left = "-560px"
      cardMask.style.visibility = "hidden"
+
 })
 
 // javascript do frame e botão
 
+const songButton = document.querySelector(".song-button")
+let songAudio = document.querySelector(".song-f1")
+
+function songPlay() {
+     songAudio.play();
+}
+
+songButton.addEventListener("click", songPlay)
 
